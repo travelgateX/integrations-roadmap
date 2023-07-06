ce421d976874eab1d18f02caec83ba0c67727408
Daniel Calle Rosa <daniel.calle@travelgate.com>
2023-07-06 13:14:09 +0200

    Create roadmapQuery.js

---
+++ src/queries/roadmapQuery.js
@@ -0,0 +1,22 @@
+import { graphql } from 'gatsby';
+
+export const roadmapQuery = graphql`
+  query {
+    allRoadmapJson {
+      edges {
+        node {
+          Summary
+          Created
+          Updated
+          Status
+          Due_date
+          Start_date
+          Target_start
+          Target_end
+          Profile_Link
+          External_Description
+        }
+      }
+    }
+  }
+`;
